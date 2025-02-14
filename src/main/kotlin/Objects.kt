bcbe3ce67c41518947617a865267e5b7a76124b5
Maksim <maksimiakovenko123321@gmail.com>
2025-02-14 19:50:39 +0100

    0.4 UI update and lives logic

--- src/main/kotlin/Objects.kt
+++ src/main/kotlin/Objects.kt
@@ -4,36 +4,50 @@ import javafx.scene.layout.Pane
 import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
 
-
-class Objects (sceneWidth:Double, sceneHeight:Double, player:Player) {
+class Objects(sceneWidth: Double, sceneHeight: Double, player: Player) {
     val root = Pane()
     private val floors = mutableListOf<Floor>()
     private val random = kotlin.random.Random
     private var sceneWith = sceneWidth
     private var sceneHeight = sceneHeight
 
-
     init {
-        addFloorPlayer(player)
+        createFloors(player)
     }
 
-    fun addFloorPlayer(player: Player) {
-        val floorX = 0.0
-        val floorY = player.cubeSize.y + player.cubeSize.height
-        val floorWidth = sceneWith
-        val floorHeight = sceneHeight
+    private fun createFloors(player: Player) {
+        val mainFloor = Floor(
+            playerY = player.cubeSize.y,
+            playerHeight = player.cubeSize.height,
+            sceneWidth = sceneWith,
+            sceneHeight = sceneHeight
+        )
+        floors.add(mainFloor)
+        root.children.add(mainFloor.root)
 
-        val floor = Floor(floorX, floorY, floorHeight, floorWidth)
-        floors.add(floor)
-        root.children.add(floor.getRectangle())
+        GameScene.floors.add(mainFloor)
     }
 
     fun createBall(): Circle {
-        val ball = Circle(random.nextDouble(5.0, 40.0)).apply {
+        val minRadius = 5.0
+        val maxRadius = 40.0
+        val radius = random.nextDouble(minRadius, maxRadius)
+
+        val minX = radius
+        val maxX = sceneWith - radius
+
+        val validX = if (minX > maxX) sceneWith / 2 else random.nextDouble(minX, maxX)
+
+        val ball = Circle(radius).apply {
             fill = Color.RED
             centerY = 0.0
-            centerX = random.nextDouble((0.0 + (radius / 2)), (sceneWith - (radius / 2)))
+            centerX = validX
         }
+
         return ball
+    }
+
+    fun updateFloor(lives: Int) {
+        GameScene.score.updateLives(lives)
     }
 }
